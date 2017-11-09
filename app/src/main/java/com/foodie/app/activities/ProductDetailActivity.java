3aa7c93a2d802f66d14c5619beac7e4b84b852d8
Santhosh <santhosh@appoets.com>
2017-11-09 18:47:20 +0530

    hotel view image slider

--- app/src/main/java/com/foodie/app/activities/ProductDetailActivity.java
+++ app/src/main/java/com/foodie/app/activities/ProductDetailActivity.java
@@ -1,30 +1,82 @@
 package com.foodie.app.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.foodie.app.R;
+import com.foodie.app.adapter.SliderPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.product_slider)
+    ViewPager productSlider;
+    @BindView(R.id.product_slider_dots)
+    LinearLayout productSliderDots;
+
+    SliderPagerAdapter sliderPagerAdapter;
+    List<String> slider_image_list;
+    int page_position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        slider_image_list = new ArrayList<>();
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/mountain_bongo_animal_mammal_220289.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/bird_mountain_bird_animal_226401.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/mountain_bongo_animal_mammal_220289.jpg");
+        slider_image_list.add("http://images.all-free-download.com/images/graphiclarge/bird_mountain_bird_animal_226401.jpg");
+        sliderPagerAdapter = new SliderPagerAdapter(this, slider_image_list);
+        productSlider.setAdapter(sliderPagerAdapter);
+        addBottomDots(0);
+        productSlider.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                addBottomDots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
             }
         });
+    }
+
+    private void addBottomDots(int currentPage) {
+        TextView[] dots = new TextView[slider_image_list.size()];
+
+        productSliderDots.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(Color.parseColor("#000000"));
+            productSliderDots.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(Color.parseColor("#FFFFFF"));
     }
 }
---
+++ app/src/main/java/com/foodie/app/adapter/SliderPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.foodie.app.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.foodie.app.R;
+
+import java.util.List;
+
+/**
+ * Created by santhosh@appoets.com on 09-11-2017.
+ */
+
+public class SliderPagerAdapter extends PagerAdapter {
+    private Activity activity;
+    private List<String> image_arraylist;
+
+    public SliderPagerAdapter(Activity activity, List<String> image_arraylist) {
+        this.activity = activity;
+        this.image_arraylist = image_arraylist;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater layoutInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(R.layout.layout_slider, container, false);
+        ImageView im_slider = view.findViewById(R.id.im_slider);
+        Glide.with(activity.getApplicationContext()).load(image_arraylist.get(position)).placeholder(R.drawable.ic_restaurant_place_holder).into(im_slider);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return image_arraylist.size();
+    }
+
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view == obj;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View) object;
+        container.removeView(view);
+    }
+}
